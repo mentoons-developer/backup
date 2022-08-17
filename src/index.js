4d38e89f18f8f13f18641dc13412124e7fb05d96
Shabeer Ahamed <shabeerahamed@Shabeers-MacBook-Air.local>
2022-08-17 20:19:52 +0530

    kl

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
 import Home4 from "./home4/Home4";
 
-createRoot(document.getElementById("root")).render(<Home4 />);
+const root = document.getElementById("root");
+createRoot(root).render(<Home4 />);
